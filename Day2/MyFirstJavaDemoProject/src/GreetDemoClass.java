a51f34bcb6e509728cd3c1cc4451743d9c37101f
Sujatabatra <sujatabatra@hotmail.com>
2023-05-10 19:18:22 +0530

    Add files via upload

---
+++ Day2/MyFirstJavaDemoProject/src/GreetDemoClass.java
@@ -0,0 +1,19 @@
+
+public class GreetDemoClass {
+
+	/*
+	  Comments are for documentation purpose ,
+	  and will not be considered/looked by the compiler
+	 */
+	public static void main(String[] args) {
+		//print() method prints the text and keeps cursor on the same line
+		System.out.print("Hello Everyone!!!");
+		/*
+		 println() method prints the text and move cursor to next line
+		 */
+		System.out.println("One more message");
+		System.out.println("Hi Line to demonstrate more");
+
+	}
+
+}
---
+++ Day2/VariableDemoProject/src/FirstDemo.java
@@ -0,0 +1,36 @@
+
+public class FirstDemo {
+
+	public static void main(String[] args) {
+		/*
+		 * Any programming Language have 
+		 * 1. Keywords : Java Reserve words
+		 * 2. Identifier : Names used to identify class,method, variable etc.
+		 * 3. Literal : Any Constant Value
+		 */
+//		int value;
+//		value=34;
+		
+		int value1=34;
+		double value2=23.4;
+//		long value3=56L;
+		long value3=56l;
+		
+//		float value4=45.3f;
+		float value4=45.3F;
+		
+		boolean value5=true;
+		
+		char value6='A';
+		
+		System.out.println("Value1 : "+value1+" Value2 : "+value2);
+		System.out.println("Values of variables are : "+value1+", "+value2);
+		
+		String value7="Chiara";
+		
+		System.out.println("Welcome "+value7+" to the world of Java!");
+		
+
+	}
+
+}
