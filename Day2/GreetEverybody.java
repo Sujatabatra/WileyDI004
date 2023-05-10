04b8f9c095e18c1f3665c568b4feaccc4ff6d936
Sujatabatra <sujatabatra@hotmail.com>
2023-05-10 15:36:39 +0530

    Add files via upload

---
+++ Day2/GreetEverybody.java
@@ -0,0 +1,7 @@
+class GreetEverybody{
+	public static void main(String args[]){
+		System.out.println("Welcome to the World of Java!!!!");
+		System.out.println("Introduced By Sujata....");
+		System.out.println("One More Line to be printed on Console");
+	}
+}
