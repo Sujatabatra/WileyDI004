45988477748aff830fe5fb328942afe8e8d248e2
Sujatabatra <sujatabatra@hotmail.com>
2023-05-11 16:27:31 +0530

    Add files via upload

---
+++ Day3/ConditionalCheckDemoProject/src/GreaterNumberClass.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class GreaterNumberClass {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		int number1,number2;
+		System.out.println("Enter First Number : ");
+		number1=scanner.nextInt();
+//		number1=Integer.parseInt(scanner.next());
+		System.out.println("Enter Second Number : ");
+		number2=scanner.nextInt();
+		/*
+		 * Conditional Check
+		 * if...else
+		 * 
+		 * switch.... case
+		 * 
+		 * Synatx of if...else
+		 * if(condition){
+		 * 	statements;  //if condition is true
+		 * }
+		 * else{
+		 * 	statements;   //if condition is false
+		 * }
+		 * 
+		 */
+		
+		if(number1>number2) {
+			System.out.println(number1+" is greater");
+		}
+		else {
+			if(number1==number2) {
+				System.out.println("Both "+number1+" and "+number2+" are equal");
+			}
+			else {
+				System.out.println(number2+" is greater");
+			}
+		}
+
+	}
+
+}
---
+++ Day3/ConditionalCheckDemoProject/src/MyFirstDemoClass.java
@@ -0,0 +1,11 @@
+
+public class MyFirstDemoClass {
+
+	public static void main(String[] args) {
+		System.out.println(10>5);
+		System.out.println(10<5);
+		
+
+	}
+
+}
