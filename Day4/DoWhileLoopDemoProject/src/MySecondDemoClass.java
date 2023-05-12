389e7ac08317391d9f0a1b7d397f4411bb9715b6
Sujatabatra <sujatabatra@hotmail.com>
2023-05-12 15:08:02 +0530

    Add files via upload

---
+++ Day4/DoWhileLoopDemoProject/src/MySecondDemoClass.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class MySecondDemoClass {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int day;
+		String choice="y";
+		while(choice.equalsIgnoreCase("y")) {
+			System.out.println("Enter Day No to display day Name : (1-7)");
+			day = scanner.nextInt();
+			switch (day) {
+			case 1:
+				System.out.println("Sunday");
+				break;
+			case 2:
+				System.out.println("Monday");
+				break;
+			case 3:
+				System.out.println("Tueday");
+				break;
+			case 4:
+				System.out.println("Wednesday");
+				break;
+			case 5:
+				System.out.println("Thursday");
+				break;
+			case 6:
+				System.out.println("Friday");
+				break;
+			case 7:
+				System.out.println("Saturday");
+				break;
+			default:
+				System.out.println("Invalid Day Number");
+			}
+
+			System.out.println("Do you wish to enter more day number to see day name");
+			choice = scanner.next();
+		} 
+		System.out.println("Good Bye, Have a Nice Day!");
+	}
+
+}
