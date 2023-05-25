0523f8baa0af44a02b790a2d7e2a3d2d6feb3055
Sujatabatra <sujatabatra@hotmail.com>
2023-05-25 16:17:54 +0530

    Add files via upload

--- Day13/EmployeeLayeredDemoProject/src/com/sujata/employee/presentation/EmployeeUserInterfaceImpl.java
+++ Day13/EmployeeLayeredDemoProject/src/com/sujata/employee/presentation/EmployeeUserInterfaceImpl.java
@@ -1,7 +1,16 @@
 package com.sujata.employee.presentation;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
+import com.sujata.employee.dto.Employee;
+import com.sujata.employee.service.EmployeeBussinessLogic;
+import com.sujata.employee.service.EmployeeBussinessLogicImpl;
+
 public class EmployeeUserInterfaceImpl implements EmployeeUserInterface {
 
+	private EmployeeBussinessLogic bussinessLogic=new EmployeeBussinessLogicImpl();
+	
 	@Override
 	public void showMenu() {
 		System.out.println("1. List All Employees");
@@ -13,17 +22,39 @@ public class EmployeeUserInterfaceImpl implements EmployeeUserInterface {
 
 	@Override
 	public void performMenu(int choice) {
+		Scanner scanner=new Scanner(System.in);
 		switch (choice) {
 		case 1: 
-			System.out.println("This module is under development");
+			LinkedList<Employee> employees=bussinessLogic.getAllEmployees();
+			for(Employee employee:employees) {
+				System.out.println(employee);
+			}
 			break;
 		case 2:
-			System.out.println("This Module is under development");
+			Employee employee=new Employee();
+			System.out.println("Enter Employee ID : ");
+			employee.setEmpId(scanner.nextInt());
+			System.out.println("Enter Employee Name : ");
+			employee.setEmpName(scanner.next());
+			System.out.println("Enter Employee Designation : ");
+			employee.setEmpDesignation(scanner.next());
+			System.out.println("Enter Employee Department : ");
+			employee.setEmpDepartment(scanner.next());
+			System.out.println("Enter Employee Salary : ");
+			employee.setEmpSalary(scanner.nextDouble());
+			
+			boolean status=bussinessLogic.addEmployee(employee);
+			
+			if(status)
+				System.out.println("Employee Added!");
+			else
+				System.out.println("Employee Not Added!");
 			break;
 		case 3:
 			System.out.println("This Module is under development");
 			break;
 		case 4:
+			bussinessLogic.saveAllEmployees();
 			System.out.println("Thanks for Using Employee Management System");
 			System.exit(0);
 			
---
+++ Day13/EmployeeLayeredDemoProject/src/com/sujata/employee/service/EmployeeBussinessLogicImpl.java
@@ -0,0 +1,52 @@
+package com.sujata.employee.service;
+
+import java.util.LinkedList;
+
+import com.sujata.employee.dto.Employee;
+import com.sujata.employee.persistence.EmployeeDataAccess;
+import com.sujata.employee.persistence.EmployeeDataAccessImpl;
+
+public class EmployeeBussinessLogicImpl implements EmployeeBussinessLogic {
+
+	
+	private EmployeeDataAccess dataAccess=new EmployeeDataAccessImpl();
+	
+	private LinkedList<Employee> employeeList;
+	
+	public EmployeeBussinessLogicImpl() {
+		try {
+			employeeList=dataAccess.readRecords();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	@Override
+	public LinkedList<Employee> getAllEmployees() {
+		
+		return employeeList;
+	}
+
+	@Override
+	public boolean addEmployee(Employee employee) {
+		
+		return employeeList.add(employee);
+	}
+
+	@Override
+	public boolean deleteEmployee(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	public void saveAllEmployees() {
+		try {
+			dataAccess.writeRecords(employeeList);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+
+}
