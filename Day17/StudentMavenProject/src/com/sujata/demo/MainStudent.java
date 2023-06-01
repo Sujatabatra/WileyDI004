2a89b494e1ae9a286521e456969efc69ed130bda
Sujatabatra <sujatabatra@hotmail.com>
2023-06-01 17:01:06 +0530

    Add files via upload

---
+++ Day17/StudentMavenProject/src/com/sujata/demo/MainStudent.java
@@ -0,0 +1,13 @@
+package com.sujata.demo;
+
+public class MainStudent {
+
+	public static void main(String[] args) {
+		Student student=new Student(101, "AAAA", 12, 89, 90, 67);
+		System.out.println(student);
+		
+		
+
+	}
+
+}
---
+++ Day17/StudentMavenProject/src/com/sujata/demo/Student.java
@@ -0,0 +1,108 @@
+package com.sujata.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+//@NoArgsConstructor
+@AllArgsConstructor
+//@ToString
+//@EqualsAndHashCode
+@Data
+public class Student {
+
+//	@Setter
+//	@Getter
+	private int rollNo;
+//	@Setter
+//	@Getter
+	private String name;
+//	@Setter
+//	@Getter
+	private int age;
+//	@Setter
+//	@Getter
+	private int marks1;
+//	@Setter
+//	@Getter
+	private int marks2;
+//	@Setter
+//	@Getter
+	private int marks3;
+	
+//	public Student() {
+//		
+//	}
+//
+//	public Student(int rollNo, String name, String age, int marks1, int marks2, int marks3) {
+//		super();
+//		this.rollNo = rollNo;
+//		this.name = name;
+//		this.age = age;
+//		this.marks1 = marks1;
+//		this.marks2 = marks2;
+//		this.marks3 = marks3;
+//	}
+//	
+//	public int getRollNo() {
+//		return rollNo;
+//	}
+//
+//	public void setRollNo(int rollNo) {
+//		this.rollNo = rollNo;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getAge() {
+//		return age;
+//	}
+//
+//	public void setAge(String age) {
+//		this.age = age;
+//	}
+//
+//	public int getMarks1() {
+//		return marks1;
+//	}
+//
+//	public void setMarks1(int marks1) {
+//		this.marks1 = marks1;
+//	}
+//
+//	public int getMarks2() {
+//		return marks2;
+//	}
+//
+//	public void setMarks2(int marks2) {
+//		this.marks2 = marks2;
+//	}
+//
+//	public int getMarks3() {
+//		return marks3;
+//	}
+//
+//	public void setMarks3(int marks3) {
+//		this.marks3 = marks3;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "Student [rollNo=" + rollNo + ", name=" + name + ", age=" + age + ", marks1=" + marks1 + ", marks2="
+//				+ marks2 + ", marks3=" + marks3 + "]";
+//	}
+//
+//	
+	
+	
+}
