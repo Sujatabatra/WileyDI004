be17a313235b4dac493033fe6e86debf4e896c20
Sujatabatra <sujatabatra@hotmail.com>
2023-06-02 14:56:15 +0530

    Add files via upload

---
+++ Day18/MyFirstSpringProject/src/com/sujata/training/Calculator.java
@@ -0,0 +1,24 @@
+package com.sujata.training;
+
+import com.sujata.demo.Operator;
+import com.sujata.demo.Product;
+
+public class Calculator {
+
+	//Depedency Logic
+	/*
+	 * IOC : Inversion of Control => separating the dependency logic from the actual logic of your application
+	 * with the help of Dependency Injection
+	 */
+	private Operator operator;
+	
+	public Calculator(Operator operator) {
+		super();
+		this.operator = operator;
+	}
+
+	public void showResult(int number1,int number2) {
+		long result=operator.operate(number1, number2);
+		System.out.println("Result : "+result);
+	}
+}
