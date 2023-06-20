e587c3dfd473cce7208ac928c6bbefea25501528
Sujatabatra <sujatabatra@hotmail.com>
2023-06-20 16:57:07 +0530

    Add files via upload

---
+++ Day30/second.js
@@ -0,0 +1,29 @@
+var x;
+x=null;
+// console.log(typeof x);
+
+x={};
+// console.log(typeof x);
+//JSON : Java Script Object Notation
+x={
+  "firstname":"Sujata",
+  "lastname":"Batra",
+  "yearsofexperience":23,
+  "address":{
+    "houseNo":111,
+    "StreetName":"Sujata Street",
+    "city":"Delhi",
+    "state":"Delhi",
+    "country":"India"
+  },
+  "prop1":true
+  
+}
+console.log(x.lastname);
+// console.log(window);
+// console.log("typeof x : "+typeof x);
+// console.log("typeof firstName : "+typeof x.firstname);
+// console.log("typeof lastname :"+typeof x.lastname);
+// // console.log("typeof yearsofexperience :"+typeof x.yearsofexperience);
+// console.log("typeof address : "+typeof x.address);
+// console.log("typeof prop1 : "+typeof x.prop1);
---
+++ Day30/variable types.js
@@ -0,0 +1,17 @@
+var x;
+// console.log(typeof x);
+x=10;
+console.log(typeof x);
+x="Sujata"
+console.log(typeof x);
+x=7.8;
+console.log(typeof x);
+x=true;
+console.log(typeof x);
+
+x=function(){
+  console.log("hello");
+}
+
+console.log(typeof x);
+
